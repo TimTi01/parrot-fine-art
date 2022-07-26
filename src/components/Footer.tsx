dbb7b40045edd244a21c34a1f029246c9d4d529b
TimTi01 <TimF1234@yandex.ru>
2022-07-26 18:29:09 +0300

    added footer logo's column nav

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,20 @@
 import React, { FC } from 'react'
 import '../styles/footer.scss'
+import '../styles/container.scss'
+import { LogoWhite } from './LogoWhite'
 
 export const Footer:FC = () => {
   return (
     <footer className='footer'>
-
+        <div className='footerWrap'>
+            <div className="logoColumn">
+                <LogoWhite/>
+                <div className="logoColumnTextWrap">
+                    <span className='logoColumnText'>Contract offer</span>
+                    <span className='logoColumnText'>Card payment rules</span>
+                </div>
+            </div>
+        </div>
     </footer>
   )
 }
