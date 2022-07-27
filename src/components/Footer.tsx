7450c38abb5b7b54c08aa7e06aa46907eee1b47a
TimTi01 <TimF1234@yandex.ru>
2022-07-27 12:58:18 +0300

    added footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,19 +1,51 @@
 import React, { FC } from 'react'
 import '../styles/footer.scss'
-import '../styles/container.scss'
+import { FacebookIcon } from './icons/FacebookIcon'
+import { Instagram } from './icons/Instagram'
+import { WhatsApp } from './icons/WhatsApp'
+// import '../styles/container.scss'
 import { LogoWhite } from './LogoWhite'
+
+const text = ['Artworks', 'Landscape', 'Portrait', 'Still life', 'Painting', 'Sculpture', 'Architecture', 'Graphic arts']
+const links = ['imagine.com', '+44 (0) 35 2953 2953', 'info@imagine.com']
 
 export const Footer:FC = () => {
   return (
     <footer className='footer'>
         <div className='footerWrap'>
-            <div className="logoColumn">
+
+          <div className='wrapFirstСontainer'>
+            <div className="footerWrapLogoColumn">
                 <LogoWhite/>
-                <div className="logoColumnTextWrap">
-                    <span className='logoColumnText'>Contract offer</span>
-                    <span className='logoColumnText'>Card payment rules</span>
-                </div>
+                <ul className='logoColumnTextWrap'>
+                  <li className='textWrapItem'>Contract offer</li>
+                  <li className='textWrapItem'>Card payment rules</li>
+                </ul>
             </div>
+
+            <ul className='footerWrapMenuColumn'>
+              {text.map((item) => (
+                <li key={item} className='menuColumnWrapMenu'>{item}</li>
+              ))}
+            </ul>
+          </div>
+
+          <div className='wrapSecondСontainer'>
+            <ul className='secondСontainerСontacts'>
+              {links.map((item) => (
+                <li key={item} className='contactsLink'>{item}</li>
+              ))}
+              <div className='contactsIconsWrap'>
+                <div className='wrapContainer'>
+                  <FacebookIcon/>
+                  <Instagram/>
+                  <WhatsApp/>
+                </div>
+              </div>
+            </ul>
+
+            <span className='secondСontainerСopyright'>© IMAGINE 2021.  All rights Reserved</span>
+          </div>
         </div>
     </footer>
   )
